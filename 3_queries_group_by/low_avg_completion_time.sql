4cc10ee78db04de1a00172d534e8fa9f0fdc33f7
elyeel <elyeel@yahoo.com>
2019-08-20 02:23:55 +0000

    added queries 3

---
+++ 3_queries_group_by/low_avg_completion_time.sql
@@ -0,0 +1,10 @@
+SELECT students.name as student,
+  AVG(assignment_submissions.duration) AS average_assignment_duration,
+  AVG(assignments.duration) as average_estimated_duration
+FROM assignments
+JOIN assignment_submissions ON assignments.id = assignment_id
+JOIN students ON students.id = student_id
+WHERE students.end_date IS NULL
+GROUP BY student
+HAVING avg(assignment_submissions.duration) < avg(assignments.duration)
+ORDER BY average_assignment_duration;
